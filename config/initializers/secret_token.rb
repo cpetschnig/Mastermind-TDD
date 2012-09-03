794f8c20680deaf0aaf67e0404f4ed7f52412611
Christoph Petschnig <info@purevirtual.de>
2012-09-03 22:00:13 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mastermind::Application.config.secret_token = '521d0b35bb7a3d48f2ea7593b919ef62bee43540ba370bbe43ca27890b9a43dba921b82aa360f680ccc4a3fc6d7917659fcd83a18d19b34a81c99ed5ef163242'
